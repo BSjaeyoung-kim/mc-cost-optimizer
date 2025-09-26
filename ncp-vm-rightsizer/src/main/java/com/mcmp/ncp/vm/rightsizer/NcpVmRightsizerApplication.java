471946b6fa8372412e429b6fad133082ea987531
dongwoo.seo <kikunie10@gmail.com>
2025-09-26 13:36:10 +0900

    ncp-vm-rightsizer module init

---
+++ ncp-vm-rightsizer/src/main/java/com/mcmp/ncp/vm/rightsizer/NcpVmRightsizerApplication.java
@@ -0,0 +1,13 @@
+package com.mcmp.ncp.vm.rightsizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcpVmRightsizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NcpVmRightsizerApplication.class, args);
+    }
+
+}
